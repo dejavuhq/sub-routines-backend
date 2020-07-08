2038618f03da8e53666501556ceac243facccd15
Rubén Padilla <rubbenpad2@gmail.com>
2020-07-08 09:00:03 -0500

    [UPDATE] statview queryset to just send stats greater than or equal to current date - 1 year

--- subroutines/stats/views.py
+++ subroutines/stats/views.py
@@ -1,6 +1,10 @@
+from django.utils import timezone
+
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import mixins
 from rest_framework import viewsets
+
+from dateutil.relativedelta import relativedelta
 
 from subroutines.stats.serializers import StatSerializer
 from subroutines.stats.models import Stat
@@ -13,6 +17,11 @@ class StatViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     pagination_class = None
 
     def get_queryset(self):
-        """Limit queryset to user logged in."""
+        """
+        Limit queryset to user logged in.
+        Just retrieve stats greater than or equal to a year before.
+        """
         user = self.request.user
-        return Stat.objects.filter(user=user)
+        return Stat.objects.filter(
+            user=user, date__gte=timezone.localdate() - relativedelta(year=1)
+        )
